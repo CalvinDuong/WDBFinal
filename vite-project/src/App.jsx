946b82bddc1bcf0760287cc42cf41ca388824ba2
William Nguyen <nguyenpwilliam@berkeley.edu>
2023-11-19 16:58:30 -0800

    added code

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,37 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
-
+import React, { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fashionSense, setFashionSense] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Call the function to generate the outfit
+    generateOutfit(fashionSense);
+  };
+
+  const generateOutfit = (fashionSense) => {
+    // Logic to generate the outfit based on the user's fashion sense
+    console.log(`Generating outfit for fashion sense: ${fashionSense}`);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Describe your fashion sense:
+          <input type="text" value={fashionSense} onChange={e => setFashionSense(e.target.value)} />
+        </label>
+        <button type="submit">Generate Outfit</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
