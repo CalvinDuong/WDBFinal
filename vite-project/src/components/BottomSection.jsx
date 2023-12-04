da470f8654b9de5985d65818f8c9820b4a6cf691
William Nguyen <nguyenpwilliam@berkeley.edu>
2023-12-04 12:23:01 -0800

    added right side, still needs fixing up

--- vite-project/src/components/BottomSection.jsx
+++ vite-project/src/components/BottomSection.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import StyleSection from './styleSection';
 import FitSection from './fitSection';
 import PersonalStyle from './PersonalStyle';
+import ColoredButton from './ColoredButton';
 import './BottomSection.css';
 
 const BottomSection = () => {
@@ -9,15 +10,20 @@ const BottomSection = () => {
         <div className='bottom-section'>
             <div className='left'>
                 <div className='style-fit-buttons-container'>
-                    <StyleSection/>
-                    <FitSection/>
+                    <StyleSection />
+                    <div className='random'></div>
+                    <FitSection />
                 </div>
                 <div className='personal-style'>
-                    <PersonalStyle/>
+                    <PersonalStyle />
                 </div>
             </div>
             <div className='right'>
-
+                <img src="https://hard-drive.net/wp-content/uploads/2023/04/yoshi.jpg.webp" alt="" className='outfit' />
+                <div className='discard-save-buttons'>
+                    <ColoredButton color="#FF6060" text="Discard" fontSize="20px" />
+                    <ColoredButton color="#71DC4C" text="Save Outfit" fontSize="20px" />
+                </div>
             </div>
 
         </div>
